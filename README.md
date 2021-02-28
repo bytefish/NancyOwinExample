c82818005dd95d8cc06520a865825d95b9d4f9f2
Philipp Wagner <pwag86@googlemail.com>
2021-02-28 08:42:58 +0100

    Re-Add Accidentally Deleted Project

---
+++ README.md
@@ -0,0 +1,5 @@
+# TokenAuthentication #
+
+The project implements Token Authentication with Nancy and Owin:
+
+* [http://bytefish.de/blog/token_authentication_owin_nancy](http://bytefish.de/blog/token_authentication_owin_nancy/)
---
+++ TokenAuthentication/TokenAuthentication/Infrastructure/Authentication/Constants.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Philipp Wagner. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace TokenAuthentication.Infrastructure.Authentication
+{
+    public static class SampleClaimTypes
+    {
+        public const string Admin = "urn:sample:admin";
+    }
+}
